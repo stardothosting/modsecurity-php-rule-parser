77af5f83aed1b5e753ed2d03ea1321fd1acbc541
SDH <admin@stardothosting.com>
2024-04-04 13:19:56 -0400

    Example script update

--- examples/example.php
+++ examples/example.php
@@ -2,6 +2,8 @@
 
 // Require Composer's autoloader
 require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../src/ModSecurityRuleParser.php';
+
 
 // Define the namespace of your package's classes
 use Stardothosting\ModSecurityParser\ModSecurityParser;
@@ -12,4 +14,3 @@ $parser = new ModSecurityParser($openaiApiKey);
 $directory = '/etc/caddy/wordpress-modsecurity-ruleset/';
 echo $parser->parseModSecurityFiles($directory);
 
-?>
